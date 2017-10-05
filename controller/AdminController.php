9a27e0c5d133fa2df7c6f8b186244a416d51742e
kevinliposl <kevinliposl@gmail.com>
2017-10-05 10:20:22 -0600

    1

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -20,4 +20,18 @@ class AdminController {
         echo json_encode($result);
     }
 
+    public function selectAdmin() {
+        require 'model/AdminModel.php';
+        $model = new AdminModel();
+
+        $id = $_POST["id"];
+        $email = $_POST["email"];
+        $name = $_POST["name"];
+        $firstLastName = $_POST["firstLastName"];
+        $secondLastName = $_POST["secondLastName"];
+
+        $result = $model->insertAdmin($id, $email, $name, $firstLastName, $secondLastName);
+        echo json_encode($result);
+    }
+
 }
