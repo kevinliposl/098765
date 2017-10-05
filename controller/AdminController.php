eb0e679c02f6704ad8812fbde83793af28137894
SilviaCalderon <31612253+SilviaCalderon@users.noreply.github.com>
2017-10-05 09:43:20 -0600

    insert professor
    insert

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -20,6 +20,25 @@ class AdminController {
         echo json_encode($result);
     }
     
+    public function insertProfessor() {
+        require 'model/AdminModel.php';
+        $model = new AdminModel();
+        $typeId = $_POST["typeId"];
+        $id = $_POST["id"];
+        $email = $_POST["email"];
+        $name = $_POST["name"];
+        $firstLastName = $_POST["firstLastName"];
+        $secondLastName = $_POST["secondLastName"];
+        $gender = $_POST["gender"];
+        $nationality = $_POST["nationality"];
+        $phone = $_POST["phone"];
+        $phone2 = $_POST["phone2"];
+        $additionalInformation = $_POST["additionalInformation"];
+        
+        $result = $model->insertProfessor($typeId, $id, $name, $firstLastName, $secondLastName, "", "", "", $gender, $phone, $phone2, $email, $additionalInformation);
+        echo json_encode($result);
+    }
+    
     public function insertTeacherView(){
         $this->view->show("insertTeacherView.php");
     }
