514beb17ac60f2cfdee1b51f3951d8269c5f36bc
brogud2694 <brogud2694@hotmail.com>
2017-11-03 13:09:58 -0600

    Fix
     UpdateStudent, ReactivateProfessor and Student and new view getStudent Data

--- controller/StudentController.php
+++ controller/StudentController.php
@@ -38,7 +38,7 @@ class StudentController {
             $this->view->show("reactivateStudentView.php", $result);
         } else {
             $model = new StudentModel();
-            $result = $model->reactivateStudent($_POST['id']);
+            $result = $model->reactivateStudent($_POST['id'], $_POST['tipo']);
             echo json_encode($result);
         }
     }
@@ -53,6 +53,14 @@ class StudentController {
             $model = new StudentModel();
             $result = $model->updateStudent($_POST["id"],$_POST["oldId"], $_POST['idType'], $_POST["email"], $_POST["name"], $_POST["firstLastName"], $_POST["secondLastName"], $_POST["age"], " ", $_POST["gender"], $_POST["nationality"], $_POST["phoneOne"], $_POST["phoneTwo"], $_POST["contactName"], $_POST["contactRelationship"], $_POST["contactPhone"], $_POST["contactEmail"]);
             echo json_encode($result);
+        }
+    }
+    public function getStudentData() {
+        require 'model/StudentModel.php';
+        if (!isset($_POST["id"]) && !isset($_POST["email"])) {
+            $model = new StudentModel();
+            $result = $model->selectAllStudent();
+            $this->view->show("getStudentDataView.php", $result);
         }
     }
 
@@ -70,9 +78,7 @@ class StudentController {
         require 'model/StudentModel.php';
         $model = new StudentModel();
 
-        $id = $_POST["id"];
-
-        $result = $model->selectDeleteStudent($id);
+        $result = $model->selectDeleteStudent($_POST["id"], $_POST["tipo"]);
         echo json_encode($result);
     }
 
