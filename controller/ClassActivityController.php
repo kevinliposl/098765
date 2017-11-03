927eb3a3d1fe1d9a1a9a7c75b2f1d83de88d3143
Diego2696 <dfonseca2696@gmail.com>
2017-11-03 11:23:40 -0600

    a

--- controller/ClassActivityController.php
+++ controller/ClassActivityController.php
@@ -34,6 +34,31 @@ class ClassActivityController {
         echo json_encode($result);
     }
     
+    public function selectConsecutiveClassActivity(){
+            require 'model/ClassActivityModel.php';
+            $model = new ClassActivityModel();
+            $result = $model->selectConsecutiveClassActivity($_POST['appointment'],$_POST['identification']);
+        echo json_encode($result);
+    }
+    
+    public function prueba(){
+            require 'model/ClassActivityModel.php';
+            $model = new ClassActivityModel();
+                        $professors = "";
+            $num_professors = 0;
+            foreach ($_POST['datos'] as $valorActual) {
+//                if ($professors == '') {
+//                    $professors= $valorActual;
+//                } else {
+//                    $professors = $valorActual . "," . $professors;
+//                }
+                $professors = $valorActual . "," . $professors;
+                $num_professors = $num_professors+ 1;
+            }//for 
+            $result = $model->prueba($num_professors,$professors);
+        echo json_encode($result);
+    }
+    
     public function insert() {
 //        if (isset($_POST["initials"]) && isset($_POST["name"]) && isset($_POST["description"]) && isset($_POST["instrument"])) {
 //            require 'model/CourseModel.php';
