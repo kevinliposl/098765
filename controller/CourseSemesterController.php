133bbc0af7acaa8dac7d2da104b2624b776aa709
Diego2696 <dfonseca2696@gmail.com>
2017-11-04 20:54:50 -0600

    a

--- controller/CourseSemesterController.php
+++ controller/CourseSemesterController.php
@@ -7,13 +7,13 @@ class CourseSemesterController {
     }
     
     public function insert() {
-        if(isset($_POST['professors']) && isset($_POST['ID_Semester']) && isset($_POST['initials'])){  
+        if(isset($_POST['professors']) && isset($_POST['ID_Semester']) && isset($_POST['initials']) && SSession::getInstance()->permissions == 'A'){  
             require 'model/CourseSemesterModel.php';
             $model = new CourseSemesterModel();          
             $professors = "";
             $num_professors = 0;
             foreach ($_POST['professors'] as $valorActual) {
-                if ($professors == '') {
+                if ($professors == '' || $professors == '-1') {
                     $professors= $valorActual;
                 } else {
                     $professors = $valorActual . "," . $professors;
@@ -31,35 +31,51 @@ class CourseSemesterController {
     }//insert  
     
     public function select() {
+        if(SSession::getInstance()->permissions == 'A'){
             require 'model/SemesterModel.php';
             $model = new SemesterModel();
             $result = $model->selectAll();
             $this->view->show("selectCourseSemesterView.php", $result);
+        }else{
+            $this->view->show("indexView.php");
+        }
     }//insert  
     
     public function selectAllCoursesSemester() {
+        if(isset($_POST['ID_Semester']) && SSession::getInstance()->permissions == 'A'){
             require 'model/CourseSemesterModel.php';
             $model = new CourseSemesterModel();
             $result = $model->selectAllCourseSemester($_POST['ID_Semester']);
             echo json_encode($result);
+        }else{
+            $this->view->show("indexView.php");
+        }
     }//insert 
 
     public function selectNotAllProfessorsCourseSemester() {
+        if(isset($_POST['ID_Semester']) && isset($_POST['initials']) && SSession::getInstance()->permissions == 'A'){
             require 'model/CourseSemesterModel.php';
             $model = new CourseSemesterModel();
             $result = $model->selectNotAllProfessorsCourseSemester($_POST['ID_Semester'],$_POST['initials']);
             echo json_encode($result);
+        }else{
+            $this->view->show("indexView.php");
+        }
     }//insert  
     
     public function selectAllProfessorsCourseSemester() {
+        if(isset($_POST['ID_Semester']) && isset($_POST['initials']) && SSession::getInstance()->permissions == 'A'){
             require 'model/CourseSemesterModel.php';
             $model = new CourseSemesterModel();
             $result = $model->selectAllProfessorsCourseSemester($_POST['ID_Semester'],$_POST['initials']);
             echo json_encode($result);
+        }else{
+            $this->view->show("indexView.php");
+        }
     }//insert  
     
     public function deleteCourse() {
-        if (isset($_POST["ID_Semester"]) && isset($_POST["initials"])) {
+        if (isset($_POST["ID_Semester"]) && isset($_POST["initials"]) && SSession::getInstance()->permissions == 'A') {
             require 'model/CourseSemesterModel.php';
             $model = new CourseSemesterModel();
             $result = $model->deleteCourseSemester($_POST['ID_Semester'],$_POST['initials']);
@@ -73,7 +89,7 @@ class CourseSemesterController {
     }//delete
     
     public function deleteProfessor() {
-        if (isset($_POST["ID_Semester"]) && isset($_POST["initials"]) && isset($_POST["identification"])) {
+        if (isset($_POST["ID_Semester"]) && isset($_POST["initials"]) && isset($_POST["identification"]) && SSession::getInstance()->permissions == 'A') {
             require 'model/CourseSemesterModel.php';
             $model = new CourseSemesterModel();
             $result = $model->deleteProfessorCourseSemester($_POST['ID_Semester'],$_POST['initials'],$_POST['identification']);
