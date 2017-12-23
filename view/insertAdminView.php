dadd79c0da49ef206b3a1f0f77059b2ae63b4de1
kevinliposl <kevinliposl@gmail.com>
2017-12-23 10:42:39 -0600

    asd

--- view/insertAdminView.php
+++ view/insertAdminView.php
@@ -1,4 +1,5 @@
 <?php
+
 $session = SSession::getInstance();
 
 if (isset($session->permissions)) {
@@ -27,38 +28,38 @@ if (isset($session->permissions)) {
                             <div class="col_full">
                                 <label for="form-id">Cedula:</label>
                                 <input type="text" id="form-id" class="form-control" required minlength="9" placeholder="304440555"/>
-                                <input type="hidden" id="failed-id" data-notify-type="error" data-notify-position="bottom-full-width" />
+                                <input type="hidden" id="failed-id" data-notify-type="error" data-notify-position="bottom-full-width" data-notify-msg="<i class='icon-remove-sign'></i> Cedula Incorrecta. Complete e intente de nuevo!"/>
                             </div>
 
                             <div class="col_full">
                                 <label for="form-email">Correo Electronico:</label>
                                 <input type="email" id="form-email" class="form-control" required placeholder="hola@dominio.com"/>
-                                <input type="hidden" id="failed-email" data-notify-type= "error" data-notify-position="bottom-full-width"/>
+                                <input type="hidden" id="failed-email" data-notify-type= "error" data-notify-position="bottom-full-width" data-notify-msg="<i class='icon-remove-sign'></i> Correo Incorrecto. Complete e intente de nuevo!"/>
                             </div>
 
                             <div class="col_full">
                                 <label for="form-name">Nombre:</label>
                                 <input type="text" id="form-name" class="form-control" required placeholder="Laura"/>
-                                <input type="hidden" id="failed-name" data-notify-type= "error" data-notify-position="bottom-full-width"/>
+                                <input type="hidden" id="failed-name" data-notify-type= "error" data-notify-position="bottom-full-width" data-notify-msg= "<i class='icon-remove-sign'></i> Nombre Incorrecto. Complete e intente de nuevo!"/>
                             </div>
 
                             <div class="col_full">
                                 <label for="form-firstlastname">Primer Apellido:</label>
                                 <input type="text" id="form-firstLastName"class="form-control" required placeholder="Brenes"/>
-                                <input type="hidden" id="failed-firstLastName" data-notify-type="error" data-notify-position="bottom-full-width"/>
+                                <input type="hidden" id="failed-firstLastName" data-notify-type="error" data-notify-position="bottom-full-width" data-notify-msg="<i class='icon-remove-sign'></i> Primer Apellido Incorrecto. Complete e intente de nuevo!"/>
                             </div>
 
                             <div class="col_full">
                                 <label for="form-secondlastname">Segundo Apellido:</label>
                                 <input type="text" id="form-secondLastName" class="form-control" required placeholder="Mora"/>
-                                <input type="hidden" id="failed-secondLastName" data-notify-type= "error" data-notify-position="bottom-full-width"/>
+                                <input type="hidden" id="failed-secondLastName" data-notify-type= "error" data-notify-position="bottom-full-width" data-notify-msg="<i class='icon-remove-sign'></i> Segundo Apellido Incorrecto. Complete e intente de nuevo!"/>
                             </div>
 
                             <div class="col_full nobottommargin">                      
                                 <input type="submit" id="submit" value="Registrar" class="button button-3d button-black nomargin form-control" style="display: block; text-align: center;"/>
-                                <input type="hidden" id="warning"/>
-                                <input type="hidden" id="success" data-notify-type="success" data-notify-msg="<i class='icon-ok-sign'></i> Operacion Exitosa!" data-notify-position="bottom-full-width"/>
-                                <input type="hidden" id="wait"/>
+                                <input type="hidden" id="warning" data-notify-type="warning" data-notify-msg="<i class='icon-warning-sign'></i>La operacion no se pudo realizar, intente de nuevo o m&aacute;s tarde!" data-notify-position="bottom-full-width"/>
+                                <input type="hidden" id="success" data-notify-type="success" data-notify-msg="<i class='icon-ok-sign'></i> Operaci&oacute;n exitosa, revise en breve...!" data-notify-position="bottom-full-width"/>
+                                <input type="hidden" id="wait" data-notify-type="info" data-notify-msg="<i class=icon-info-sign></i> Espere un momento...!" data-notify-position="bottom-full-width"/>
                             </div>
                         </form>
                     </div>
@@ -102,27 +103,22 @@ if (isset($session->permissions)) {
         secondLastName = $("#form-secondLastName").val().trim();
 
         if (!/\w+@\w+\.+[a-z]/.test(email) || email.split(" ", 2).length > 1) {
-            $("#failed-email").attr("data-notify-msg", "<i class=icon-remove-sign></i> Correo Incorrecto. Complete e intente de nuevo!");
             SEMICOLON.widget.notifications($("#failed-email"));
             return false;
 
         } else if (isNaN(id) || id.length < 9 || id.split(" ", 2).length > 1) {
-            $("#failed-id").attr("data-notify-msg", "<i class=icon-remove-sign></i> Cedula Incorrecta. Complete e intente de nuevo!");
             SEMICOLON.widget.notifications($("#failed-id"));
             return false;
 
         } else if (name.length < 4 || name.split(" ", 2).length > 1) {
-            $("#failed-name").attr("data-notify-msg", "<i class=icon-remove-sign></i> Nombre Incorrecto. Complete e intente de nuevo!");
             SEMICOLON.widget.notifications($("#failed-name"));
             return false;
 
         } else if (firstLastName.length < 4 || firstLastName.split(" ", 2).length > 1) {
-            $("#failed-firstLastName").attr("data-notify-msg", "<i class=icon-remove-sign></i> Primer Apellido Incorrecto. Complete e intente de nuevo!");
             SEMICOLON.widget.notifications($("#failed-firstLastName"));
             return false;
 
         } else if (secondLastName.length < 4 || secondLastName.split(" ", 2).length > 1) {
-            $("#failed-secondLastName").attr("data-notify-msg", "<i class=icon-remove-sign></i> Segundo Apellido Incorrecto. Complete e intente de nuevo!");
             SEMICOLON.widget.notifications($("#failed-secondLastName"));
             return false;
         }
@@ -134,6 +130,8 @@ if (isset($session->permissions)) {
     $("#form-submity").click(function () {
         $("#form-submity").attr('disabled', 'disabled');
         $("#form-close").attr('disabled', 'disabled');
+        
+        SEMICOLON.widget.notifications($("#wait"));
 
         var parameters = {
             "id": $("#form-id").val().trim(),
@@ -142,16 +140,12 @@ if (isset($session->permissions)) {
             "firstLastName": $("#form-firstLastName").val().trim(),
             "secondLastName": $("#form-secondLastName").val().trim()
         };
+
         $.post("?controller=Admin&action=insert", parameters, function (data) {
             if (data.result === "1") {
                 SEMICOLON.widget.notifications($("#success"));
                 setTimeout("location.href = '?';", 1000);
             } else {
-                $("#warning").attr({
-                    "data-notify-type": "warning",
-                    "data-notify-msg": "<i class=icon-warning-sign></i> El Administrador ya existe en el Sistema!",
-                    "data-notify-position": "bottom-full-width"
-                });
                 SEMICOLON.widget.notifications($("#warning"));
             }
             ;
@@ -164,4 +158,5 @@ if (isset($session->permissions)) {
 <!-- End Content
 ============================================= -->    
 <?php
+
 include_once 'public/footer.php';
