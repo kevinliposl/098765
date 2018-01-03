d4e40e56b371d873bd88e8da4d5cafb5900e17d1
kevinliposl <kevinliposl@gmail.com>
2018-01-03 14:38:31 -0600

    asd

--- view/insertProfessorView.php
+++ view/insertProfessorView.php
@@ -33,7 +33,7 @@ if (isset($session->permissions)) {
                         </div>
 
                         <div class="col-lg-6" style="padding: 10px;">
-                            <label for="form-id">Número de Identificaci&oacute;n:    (PONER PATTERN CON $(#).attr() dependiendo del radio seleccionado)</label>
+                            <label for="form-id">Número de Identificaci&oacute;n:</label>
                             <input type="text" id="form-id" class="form-control" required/>
                             <input type="hidden" id="failed-id" data-notify-type= "error" data-notify-position="bottom-full-width"/>
                         </div>
@@ -61,10 +61,10 @@ if (isset($session->permissions)) {
                             <input type="radio" name="form-gender" value="M" checked/> <label>Masculino</label>
                             <input type="radio" name="form-gender" value="F"/><label>Femenino</label>
                         </div>
-
-                        <div class="col-lg-6" style="padding: 10px;">
+                        
+                        <div class="col_full" style="padding: 10px;">
                             <label for="form-address">Lugar de Residencia:</label>
-                            <input type="text" id="form-address" class="form-control" pattern="[a-zA-Z\s]+$" placeholder="Garcia" minlength="5" maxlength="200" required/>
+                            <input type="text" id="form-address" class="form-control" pattern="[a-zA-Z\s]+$" minlength="5" maxlength="200" required/>
                             <input type="hidden" id="failed-address" data-notify-type= "error" data-notify-position="bottom-full-width" data-notify-msg="<i class=icon-remove-sign></i> Dirección muy extensa. Complete e intente de nuevo!"/>
                         </div>
 
@@ -88,7 +88,7 @@ if (isset($session->permissions)) {
 
                         <div class="col-lg-6" style="padding: 10px;">
                             <label for="form-email">Correo Electr&oacute;nico:</label>
-                            <input type="email" id="form-email" class="form-control" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required/>
+                            <input type="text" id="form-email" class="form-control" required/>
                             <input type="hidden" id="failed-email" data-notify-type= "error" data-notify-position="bottom-full-width"/>
                         </div>
 
@@ -144,52 +144,50 @@ if (isset($session->permissions)) {
 <script>
     function val() {
 
-        args = {
-            "typeId": $("input[name*='form-typeId']").val(),
-            "id": $("#form-id").val().trim(),
-            "email": $("#form-email").val().trim(),
-            "name": $("#form-name").val().trim(),
-            "firstLastName": $("#form-firstLastName").val().trim(),
-            "secondLastName": $("#form-secondLastName").val().trim(),
-            "gender": $("input[name*='form-gender']").val(),
-            "nationality": $("#form-nationality").val().trim(),
-            "phone": $("#form-phone1").val().trim(),
-            "phone2": $("#form-phone2").val().trim(),
-            "additionalInformation": $("#form-additionalInformation").val().trim(),
-            "address": $("#form-address").val().trim(),
-            "age": $("#form-age").val()
-        };
+            var typeId = $("input[name*='form-typeId']").val();
+            var id = $("#form-id").val().trim();
+            var email= $("#form-email").val().trim();
+            var name = $("#form-name").val().trim();
+            var firstLastName= $("#form-firstLastName").val().trim();
+            var secondLastName= $("#form-secondLastName").val().trim();
+            var gender = $("input[name*='form-gender']").val();
+            var nationality = $("#form-nationality").val().trim();
+            var phone = $("#form-phone1").val().trim();
+            var phone2 = $("#form-phone2").val().trim();
+            var additionalInformation = $("#form-additionalInformation").val().trim();
+            var address = $("#form-address").val().trim();
+            var age = $("#form-age").val();
+        
 
-
-        if (args['name'].length < 3 || args['name'].length > 49 || !isNaN(args['name'])) {
+        if (name.length < 3 || name.length > 49 || !isNaN(name)) {
             SEMICOLON.widget.notifications($("#failed-name"));
             return false;
 
-        } else if (args['firstLastName'].length < 3 || args['firstLastName'].length > 49 || !isNaN(args['firstLastName'])) {
+        } else if (firstLastName.length < 3 || firstLastName.length > 49 || !isNaN(firstLastName)) {
             SEMICOLON.widget.notifications($("#failed-firsLastName"));
             return false;
 
-        } else if (args['secondLastName'].length < 3 || args['secondLastName'].length > 49 || !isNaN(args['secondLastName'])) {
+        } else if (secondLastName.length < 3 || secondLastName.length > 49 || !isNaN(secondLastName)) {
             SEMICOLON.widget.notifications($("#failed-secondLastName"));
             return false;
 
-        } else if (args['phone'].length < 8 || args['phone'].length > 8 || isNaN(args['phone'])) {
+        } else if (phone.length < 8 || phone.length > 8 || isNaN(phone)) {
             SEMICOLON.widget.notifications($("#failed-phone1"));
             return false;
 
-        } else if (args['phone2'].length < 8 || args['phone2'].length > 8 || isNaN(args['phone2'])) {
+        } else if (phone2.length < 8 || phone2.length > 8 || isNaN(phone2)) {
             SEMICOLON.widget.notifications($("#failed-phone2"));
             return false;
 
-        } else if (args['nationality'].length < 6 || args['nationality'].length > 49 || !isNaN(args['nationality']) || args['nationality'].split(" ", 2).length > 1) {
+        } else if (nationality.length < 6 || nationality.length > 49 || !isNaN(nationality) || nationality.split(" ", 2).length > 1) {
             SEMICOLON.widget.notifications($("#failed-nationality"));
             return false;
 
-        } else if (args['address'].length > 200) {
+        } else if (address.length > 200) {
             SEMICOLON.widget.notifications($("#failed-address"));
             return false;
 
-        } else if (args['additionalInformation'].length > 2000) {
+        } else if (additionalInformation.length > 2000) {
             SEMICOLON.widget.notifications($("#failed-additionalInformation"));
             return false;
 
@@ -218,11 +216,25 @@ if (isset($session->permissions)) {
         $("#form-submity").attr('disabled', 'disabled');
         $("#form-close").attr('disabled', 'disabled');
 
+        var param = {
+            "typeId": $("input[name*='form-typeId']").val(),
+            "id": $("#form-id").val().trim(),
+            "email": $("#form-email").val().trim(),
+            "name": $("#form-name").val().trim(),
+            "firstLastName": $("#form-firstLastName").val().trim(),
+            "secondLastName": $("#form-secondLastName").val().trim(),
+            "gender": $("input[name*='form-gender']").val(),
+            "nationality": $("#form-nationality").val().trim(),
+            "phone": $("#form-phone1").val().trim(),
+            "phone2": $("#form-phone2").val().trim(),
+            "additionalInformation": $("#form-additionalInformation").val().trim(),
+            "address": $("#form-address").val().trim(),
+            "age": $("#form-age").val()
+        };
+
         SEMICOLON.widget.notifications($("#wait"));
 
-        $.post("?controller=Professor&action=insert", args, function (data) {
-            
-            alert(data.result);
+        $.post("?controller=Professor&action=insert", param, function (data) {
             if (data.result === "1") {
                 SEMICOLON.widget.notifications($("#success"));
                 setTimeout("location.href = '?controller=Professor&action=insert';", 1500);
