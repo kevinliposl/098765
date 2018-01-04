bc6b8f42a1b96caf19f19cfbfeb0c2defcfc26fe
brogud2694 <brogud2694@hotmail.com>
2018-01-04 15:41:08 -0600

    h

---
+++ view/PdfExpStudentView.php
@@ -0,0 +1,150 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif; background-color:#eeeeee; margin:0; padding:0; color:#333333;">
+        <table width="100%" bgcolor="#eeeeee" cellpadding="0" cellspacing="0" border="0">
+            <tbody>
+                <tr>
+                    <td>
+                        <table cellpadding="0" cellspacing="0" width="600" border="0" align="center">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <table cellpadding="0" cellspacing="0" border="0" width="100%">
+                                            <tbody>
+                                                <tr>
+                                                    <td colspan="3" rowspan="3" bgcolor="#FFFFFF" style="padding:0">
+                                                        <p style="margin:30px 33px;; text-align:center; text-transform:uppercase; font-size:24px; line-height:50px; font-weight:bold; color:#484a42;">
+
+                                                            Expediente Acad&eacute;mico de **************
+
+                                                        </p>
+                                                        <!-- inicio articulos -->
+                                                        <table cellpadding="0" cellspacing="0" border="0" width="100%" style="border: 1px solid black;">
+                                                            <tbody>
+                                                                <tr valign="top">
+                                                                    <td width="30">
+                                                                        <p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p>
+                                                                    </td>
+                                                                    <td colspan="3">
+                                                                        <p style="font-size:18px; color:#6c7e44; text-decoration:none; line-height:22px; font-weight:bold; color:#333333; margin:0 0 5px;">
+
+                                                                            Identificaci&oacute;n
+
+                                                                        </p>
+                                                                    </td>
+                                                                    <td colspan="3">
+                                                                        <p style="font-size:16px; color:#6c7e44; text-decoration:none; line-height:22px; font-weight:bold; color:#333333; margin:0 0 5px;">
+
+                                                                            **********
+
+                                                                        </p>
+                                                                    </td>
+                                                                    <td width="30"><p style="margin:0; font-size:1px; line-height:40px;">&nbsp;</p></td>
+                                                                </tr>
+                                                                 <tr valign="top">
+                                                                    <td width="30">
+                                                                        <p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p>
+                                                                    </td>
+                                                                    <td colspan="3">
+                                                                        <p style="font-size:18px; color:#6c7e44; text-decoration:none; line-height:22px; font-weight:bold; color:#333333; margin:0 0 5px;">
+
+                                                                            Nombre Completo
+
+                                                                        </p>
+                                                                    </td>
+                                                                    <td colspan="3">
+                                                                        <p style="font-size:16px; color:#6c7e44; text-decoration:none; line-height:22px; font-weight:bold; color:#333333; margin:0 0 5px;">
+
+                                                                            **********
+
+                                                                        </p>
+                                                                    </td>
+                                                                    <td width="30"><p style="margin:0; font-size:1px; line-height:40px;">&nbsp;</p></td>
+                                                                </tr>
+                                                                <tr valign="top">
+                                                                    <td width="30">
+                                                                        <p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p>
+                                                                    </td>
+                                                                    <td colspan="3">
+                                                                        <p style="font-size:18px; color:#6c7e44; text-decoration:none; line-height:22px; font-weight:bold; color:#333333; margin:0 0 5px;">
+
+                                                                            Nombre del Contacto
+
+                                                                        </p>
+                                                                    </td>
+                                                                    <td colspan="3">
+                                                                        <p style="font-size:16px; color:#6c7e44; text-decoration:none; line-height:22px; font-weight:bold; color:#333333; margin:0 0 5px;">
+
+                                                                            **********
+
+                                                                        </p>
+                                                                    </td>
+                                                                    <td width="30"><p style="margin:0; font-size:1px; line-height:40px;">&nbsp;</p></td>
+                                                                </tr>
+                                                                <tr valign="top">
+                                                                    <td width="30">
+                                                                        <p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p>
+                                                                    </td>
+                                                                    <td colspan="3">
+                                                                        <p style="font-size:18px; color:#6c7e44; text-decoration:none; line-height:22px; font-weight:bold; color:#333333; margin:0 0 5px;">
+
+                                                                            Tel&eacute;fono del Estudiante
+
+                                                                        </p>
+                                                                    </td>
+                                                                    <td colspan="3">
+                                                                        <p style="font-size:16px; color:#6c7e44; text-decoration:none; line-height:22px; font-weight:bold; color:#333333; margin:0 0 5px;">
+
+                                                                            **********
+
+                                                                        </p>
+                                                                    </td>
+                                                                    <td width="30"><p style="margin:0; font-size:1px; line-height:40px;">&nbsp;</p></td>
+                                                                </tr>
+                                                                <tr valign="top">
+                                                                    <td width="30">
+                                                                        <p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p>
+                                                                    </td>
+                                                                    <td colspan="3">
+                                                                        <p style="font-size:18px; color:#6c7e44; text-decoration:none; line-height:22px; font-weight:bold; color:#333333; margin:0 0 5px;">
+
+                                                                            Tel&eacute;fono del Contacto
+
+                                                                        </p>
+                                                                    </td>
+                                                                    <td colspan="3">
+                                                                        <p style="font-size:16px; color:#6c7e44; text-decoration:none; line-height:22px; font-weight:bold; color:#333333; margin:0 0 5px;">
+
+                                                                            **********
+
+                                                                        </p>
+                                                                    </td>
+                                                                    <td width="30"><p style="margin:0; font-size:1px; line-height:40px;">&nbsp;</p></td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <!-- end main block -->
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
+</br>
+</div>
+</div>
+</div>
+</section>
+</body>
+</html>
