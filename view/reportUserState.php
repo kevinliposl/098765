010094574f62b048f6e78b9131b0434dc090b98b
kevinliposl <kevinliposl@gmail.com>
2018-01-07 14:52:11 -0600

    asd

--- view/reportUserState.php
+++ view/reportUserState.php
@@ -22,29 +22,37 @@ if (isset($session->permissions)) {
 <section id="content">
     <div class="content-wrap">
         <div class="container clearfix">
-            <div class="table-responsive">
-                <table id="datatable" class="table table-striped table-bordered" cellspacing="0" width="100%">
-                    <thead>
-                        <tr>
-                            <th>Rol</th>
-                            <th>Cantidad</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        foreach ($vars as $var) {
-                            if (isset($var["permissions"])) {
-                                ?>
-                                <tr>
-                                    <td><?php echo $var["permissions"] == "A" ? 'Administrador' : ($var["permissions"] == "S" ? 'Estudiante' : 'Profesor'); ?></td>
-                                    <td><?php echo $var["data"]; ?></td>
-                                </tr>
-                                <?php
+            <div id="data">
+                <div class="table-responsive">
+                    <table id="datatable" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                        <thead>
+                            <tr>
+                                <th>Rol</th>
+                                <th>Cantidad</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            foreach ($vars as $var) {
+                                if (isset($var["permissions"])) {
+                                    ?>
+                                    <tr>
+                                        <td><?php echo $var["permissions"] == "A" ? 'Administrador' : ($var["permissions"] == "S" ? 'Estudiante' : 'Profesor'); ?></td>
+                                        <td><?php echo $var["data"]; ?></td>
+                                    </tr>
+                                    <?php
+                                }
                             }
-                        }
-                        ?>
-                    </tbody>
-                </table>
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col_one_fifth">
+                <input type="button" onclick="javascript:exportPdf();" value="Exportar PDF" class="button button-3d button-black form-control"/>
+            </div>
+            <div class="col_one_fifth">
+                <input type="button" onclick="javascript:exportExcel()" value="Exportar EXCEL" class="button button-3d button-black form-control"/>
             </div>
         </div>
     </div>
@@ -55,3 +63,8 @@ if (isset($session->permissions)) {
 
 <?php
 include_once 'public/footer.php';
+?>
+
+<script src="public/js/jquery.table2excel.min.js" type="text/javascript"></script>
+<script src="public/js/pdf/html2pdf.js" type="text/javascript"></script>
+<script src="public/js/pdf/jspdf.debug.js" type="text/javascript"></script>
