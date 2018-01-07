1a8c56e885ffbd0e250cedad3c94348ca22a6944
brogud2694 <brogud2694@hotmail.com>
2018-01-07 15:51:46 -0600

    a

--- view/aboutView.php
+++ view/aboutView.php
@@ -90,49 +90,26 @@ if (isset($session->permissions)) {
         </div>
         <div class="col-sm-5 col-padding" style="background: url('images/team/8.jpg') center center no-repeat; background-size: cover;"></div>
     </div>
-    <div class="container clearfix">
-        <div class="clear"></div>
-        <div class="heading-block center">
-            <h4>Nuestros Socios</h4>
-        </div>
-
-        <ul class="clients-grid grid-6 nobottommargin clearfix">
-            <li><a href="http://logofury.com/logo/enzo.html"><img src="images/clients/1.png" alt="Clients"></a></li>
-            <li><a href="http://logofury.com"><img src="images/clients/2.png" alt="Clients"></a></li>
-            <li><a href="http://logofaves.com/2014/03/grabbt/"><img src="images/clients/3.png" alt="Clients"></a></li>
-            <li><a href="http://logofaves.com/2014/01/ladera-granola/"><img src="images/clients/4.png" alt="Clients"></a></li>
-            <li><a href="http://logofaves.com/2014/02/hershel-farms/"><img src="images/clients/5.png" alt="Clients"></a></li>
-            <li><a href="http://logofury.com/logo/food-fight-radio.html"><img src="images/clients/6.png" alt="Clients"></a></li>
-            <li><a href="http://logofury.com"><img src="images/clients/7.png" alt="Clients"></a></li>
-            <li><a href="http://logofury.com/logo/up-travel.html"><img src="images/clients/8.png" alt="Clients"></a></li>
-            <li><a href="http://logofury.com/logo/caffi-bardi.html"><img src="images/clients/9.png" alt="Clients"></a></li>
-            <li><a href="http://logofury.com/logo/bignix-design.html"><img src="images/clients/10.png" alt="Clients"></a></li>
-            <li><a href="http://logofury.com/"><img src="images/clients/11.png" alt="Clients"></a></li>
-            <li><a href="http://logofury.com/"><img src="images/clients/12.png" alt="Clients"></a></li>
-        </ul>
-    </div>
     <div class="section footer-stick">
         <h4 class="uppercase center">¿Qu&eacute; dicen nuestros <span>clientes</span>?</h4>
         <div class="fslider testimonial testimonial-full" data-animation="fade" data-arrows="false">
-            <div class="flexslider">
-                <div class="slider-wrap">
-                    <div class="slide">
-                        <div class="testi-image">
-                            <a href="#"><img src="images/testimonials/3.jpg" alt="Customer Testimonails"></a>
-                        </div>
-                        <div class="testi-content">
-                            <p>Simplemente Magn&iacute;fico</p>
-                            <div class="testi-meta">
-                                Yurlin Ruiz
-                                <span>Estudiante.</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <div id="fb-root"></div>
+            <script>(function (d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id))
+                        return;
+                    js = d.createElement(s);
+                    js.id = id;
+                    js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.11';
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));</script>
+
         </div>
     </div>
+    <div class="fb-comments" data-href="http://fusionacademiacr.com/" data-numposts="5"></div>
+    
 </section>
 
 <?php
+
 include_once 'public/footer.php';
