f329f61400bc4e0646a1b3f7dfeef7612db6185f
kevinliposl <kevinliposl@gmail.com>
2018-03-13 11:21:35 -0600

    asd

--- view/selectClassActivityView.php
+++ view/selectClassActivityView.php
@@ -123,5 +123,15 @@ if (isset($session->permissions)) {
 <script src="public/js/jquery.min.js" type="text/javascript"></script>
 <script src="public/js/Views/selectClassActivityView.js" type="text/javascript"></script>
 
+<script src="public/js/bs-select.js" type="text/javascript"></script>
+<script src="public/js/selectsplitter.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+    $('.selectpicker').selectpicker({
+        size: 4,
+        dropupAuto: false
+    });
+</script>
+
 <?php
 include_once 'public/footer.php';
--- view/updateClassActivityView.php
+++ view/updateClassActivityView.php
@@ -143,5 +143,16 @@ if (isset($session->permissions)) {
 <script src="public/js/Views/updateClassActivityView.js" type="text/javascript"></script>
 <script src="public/js/Views/general.js" type="text/javascript"></script>
 
+<script src="public/js/bs-select.js" type="text/javascript"></script>
+<script src="public/js/selectsplitter.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+    $('.selectpicker').selectpicker({
+        size: 4,
+        dropupAuto: false
+    });
+</script>
+
+
 <?php
 include_once 'public/footer.php';
