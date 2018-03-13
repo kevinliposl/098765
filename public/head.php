14c300a01ddbf9d21193f5de86a5f9ba2c40618f
kevinliposl <kevinliposl@gmail.com>
2018-03-13 11:45:16 -0600

    asd

--- public/head.php
+++ public/head.php
@@ -6,7 +6,7 @@
         <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" type="text/css" />
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" type="text/css" media="none" onload="if(media!='all')media='all'"/>
 
         <link rel="icon" href="public/images/favicon.ico" type="image/x-icon"/>
         <link rel="shortcut icon"  href="public/images/favicon.ico" type="image/x-icon"/>
@@ -18,11 +18,11 @@
         <link rel="stylesheet" href="public/css/font-icons.css" type="text/css" />
         <link rel="stylesheet" href="public/css/animate.css" type="text/css" />
         <link rel="stylesheet" href="public/css/magnific-popup.css" type="text/css" />
-        <link rel="preload" href="public/css/components/bs-select.css" type="text/css" />
-        <link rel="stylesheet" href="public/css/select2.css" type="text/css" />
-        <link rel="preload" href="public/css/components/bs-editable.css" type="text/css" />
-        <link rel="stylesheet" href="public/css/typeahead.js-bootstrap.css" type="text/css"/>
-        <link rel="preload" href="public/css/components/bs-select.css" type="text/css" />
+        <link rel="stylesheet" href="public/css/components/bs-select.css" type="text/css" media="none" onload="if(media!='all')media='all'"/>
+        <link rel="stylesheet" href="public/css/select2.css" type="text/css" media="none" onload="if(media!='all')media='all'"/>
+        <link rel="stylesheet" href="public/css/components/bs-editable.css" type="text/css" media="none" onload="if(media!='all')media='all'"/>
+        <link rel="stylesheet" href="public/css/typeahead.js-bootstrap.css" type="text/css" media="none" onload="if(media!='all')media='all'"/>
+        <link rel="stylesheet" href="public/css/components/bs-select.css" type="text/css" media="none" onload="if(media!='all')media='all'"/>
 
         <link rel="stylesheet" href="public/css/responsive.css" type="text/css" />
 
