2a6555820d4055a13c0e0c3198e10529959791a6
brogud <brogudbarrientos@gmail.com>
2018-04-08 14:48:41 -0600

    a

--- view/aboutView.php
+++ view/aboutView.php
@@ -28,68 +28,80 @@ if (isset($session->permissions)) {
         <div class="container clearfix">
             <div class="col_one_third">
                 <div class="heading-block fancy-title nobottomborder title-bottom-border">
-                    <h4>por qué <span>elegirnos</span>.</h4>
+                    <h4>¿Por qué <span>elegirnos</span>?</h4>
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quidem minus id omnis, nam expedita, ea fuga commodi voluptas iusto, hic autem deleniti dolores explicabo labore enim repellat earum perspiciatis.</p>
+                <p style="text-align: justify; text-justify: inter-word;">
+                Porque somos j&oacute;venes emprendedores, profesionales en educaci&oacute;n musical, con un enfoque moderno y diferente, fusionamos diferentes estilos, experiencias y capacidades, para potenciar el desarrollo de las habilidades musicales de nuestros estudiantes. Nuestro enfoque se sustenta en tres pilares fundamentales: el estudio te&oacute;rico pr&aacute;ctico del instrumento, el ensamble musical y la presentación en vivo ante un p&uacute;blico.
+                </p>
             </div>
             <div class="col_one_third">
                 <div class="heading-block fancy-title nobottomborder title-bottom-border">
                     <h4>Nuestra <span>mision</span>.</h4>
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quidem minus id omnis, nam expedita, ea fuga commodi voluptas iusto, hic autem deleniti dolores explicabo labore enim repellat earum perspiciatis.</p>
+                <p style="text-align: justify; text-justify: inter-word;">
+                Brindar servicios musicales de la m&aacute;s alta calidad, facilitando espacios a nuestra comunidad para el desarrollo de las habilidades musicales de niños, j&oacute;venes y adultos, a trav&eacute;s de nuestro excelente equipo de m&uacute;sicos y docentes, con un enfoque moderno y diferente.
+                </p>
             </div>
             <div class="col_one_third col_last">
                 <div class="heading-block fancy-title nobottomborder title-bottom-border">
-                    <h4>Que <span>hacemos</span>.</h4>
+                    <h4>¿Qu&eacute; <span>hacemos</span>?</h4>
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quidem minus id omnis, nam expedita, ea fuga commodi voluptas iusto, hic autem deleniti dolores explicabo labore enim repellat earum perspiciatis.</p>
+                <p style="text-align: justify; text-justify: inter-word;">
+                Lecciones musicales privadas para diferentes instrumentos, talleres, clases maestras, servicios musicales para presentaciones privadas, reparaci&oacute;n y mantenimiento de instrumentos, grabaci&oacute;n de demos musicales, producci&oacute;n de jingles, entre otros servicios.
+                </p>
             </div>
         </div>
     </div>
-    <div class="row common-height clearfix">
-        <div class="col-sm-5 col-padding" style="background: url('images/presentation/2.jpg') center center no-repeat; background-size: cover;"></div>
-        <div class="col-sm-7 col-padding">
-            <div>
-                <div class="heading-block">
-                    <span class="before-heading color">Co-fundador</span>
-                    <h3>Wagner Vargas Moya</h3>
-                </div>
+    <div class="heading-block center">
+		<h2>¿Quienes est&aacute;n detr&aacute;s de Fusi&oacute;n?</h2>
+	</div>
+    <div class="row clearfix">
+	<div class="col-md-6 bottommargin">
+		<div class="team team-list clearfix">
+			<div>
+				<img src="public/images/about/perfilWoman.jpg" alt="ESTEFANIE SANCHEZ COTO">
+			</div>
+			<div class="team-desc">
+				<div class="team-title"><h4>ESTEFANIE SANCHEZ COTO</h4><span>Co-Fundadora</span></div>
+				<div class="team-content">
+					<p style="text-align: justify; text-justify: inter-word;">
+                    Directora de Fusión Academia de Música. Emprendedora, música y docente. Licenciada en Enseñanza de la Música por la Universidad de Costa Rica, Bachiller en Interpretación y Enseñanza  de la Dirección Coral por la Universidad Nacional de Costa Rica, bajo la tutela del maestro David Ramírez. Directora coral, docente universitaria, docente para el Ministerio de Educación Pública. Integrante del grupo vocal internacional Ocho en Voz, multi instrumentista, gestora del movimiento coral en Turrialba, más de 5 años de experiencia como docente y directora coral. Directora de Coro Zafra, Coro Universitario y Coro de Niños, agrupaciones corales de la Universidad de Costa Rica, Sede del Atlántico, Turrialba. Ha recibidos clases maestras con los directores Hanz-Peter Shurz (Alemania), T.J Harper (Estados Unidos) y Cesar Alejandro Carrillo (Venezuela).
+                    </p>
+				</div>
+				<a href="https://www.facebook.com/tef.sanchezcoto" target="__blank" class="social-icon si-rounded si-small si-light si-facebook">
+					<i class="icon-facebook"></i>
+					<i class="icon-facebook"></i>
+				</a>
+			</div>
+		</div>
+	</div>
 
-                <div class="row clearfix">
-
-                    <div class="col-md-6">
-                        <p>Employment respond committed meaningful fight against oppression social challenges rural legal aid governance. Meaningful work, implementation, process cooperation, campaign inspire.</p>
-                        <p>Advancement, promising development John Lennon, our ambitions involvement underprivileged billionaire philanthropy save the world transform. Carbon rights maintain healthcare emergent, implementation inspire social change solve clean water livelihoods.</p>
-                        <a href="#" class="social-icon inline-block si-small si-light si-rounded si-facebook">
-                            <i class="icon-facebook"></i>
-                            <i class="icon-facebook"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row common-height bottommargin-lg clearfix">
-        <div class="col-sm-7 col-padding">
-            <div>
-                <div class="heading-block">
-                    <span class="before-heading color">Co-Fundador</span>
-                    <h3>Estefanie S&aacute;nchez Coto</h3>
-                </div>
-                <div class="row clearfix">
-                    <div class="col-md-6">
-                        <p>Employment respond committed meaningful fight against oppression social challenges rural legal aid governance. Meaningful work, implementation, process cooperation, campaign inspire.</p>
-                        <p>Advancement, promising development John Lennon, our ambitions involvement underprivileged billionaire philanthropy save the world transform. Carbon rights maintain healthcare emergent, implementation inspire social change solve clean water livelihoods.</p>
-                        <a href="#" class="social-icon inline-block si-small si-light si-rounded si-facebook">
-                            <i class="icon-facebook"></i>
-                            <i class="icon-facebook"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-5 col-padding" style="background: url('images/team/8.jpg') center center no-repeat; background-size: cover;"></div>
-    </div>
+	<div class="col-md-6 bottommargin">
+		<div class="team team-list clearfix">
+			<div>
+				<img src="public/images/about/perfilMan.jpg" alt="Josh Clark">
+			</div>
+			<div class="team-desc">
+				<div class="team-title"><h4>Josh Clark</h4><span>Co-Founder</span></div>
+				<div class="team-content">
+					<p>Carbon emissions reductions giving, legitimize amplify non-partisan Aga Khan. Policy dialogue assessment expert free-speech cornerstone disruptor freedom. Cesar Chavez empower.</p>
+				</div>
+				<a href="#" class="social-icon si-rounded si-small si-light si-facebook">
+					<i class="icon-facebook"></i>
+					<i class="icon-facebook"></i>
+				</a>
+				<a href="#" class="social-icon si-rounded si-small si-light si-twitter">
+					<i class="icon-twitter"></i>
+					<i class="icon-twitter"></i>
+				</a>
+				<a href="#" class="social-icon si-rounded si-small si-light si-gplus">
+					<i class="icon-gplus"></i>
+					<i class="icon-gplus"></i>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
     <div class="section footer-stick">
         <h4 class="uppercase center">¿Qu&eacute; dicen nuestros <span>clientes</span>?</h4>
         <div class="fslider testimonial testimonial-full" data-animation="fade" data-arrows="false">
